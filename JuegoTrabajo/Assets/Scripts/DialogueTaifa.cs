7b6c9bed08fffe38d699df13549958ead4e40275
rubenmujiica <rubenmujicasanchez@gmail.com>
2024-05-18 17:30:58 +0200

    Primeros Dialogos

---
+++ JuegoTrabajo/Assets/Scripts/DialogueTaifa.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class DialogueTaifa : MonoBehaviour
+{
+
+    public TextMeshProUGUI dialogueText;
+
+    public string[] lines;
+
+    public float textSpeed = 0.1f;
+
+    int index;
+
+    void Start()
+    {
+        dialogueText.text = string.Empty;
+        StartDialogue();
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(dialogueText.text == lines[index])
+            {
+                NextLine();
+            }
+            else{
+                StopAllCoroutines();
+                dialogueText.text = lines[index];
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        index = 0;
+
+        StartCoroutine(WriteLine());
+
+    }
+
+    IEnumerator WriteLine()
+    {
+        foreach(char letter in lines[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            dialogueText.text = string.Empty;
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine(WriteLine());
+        }
+        else
+        {
+            SceneManager.LoadScene("CasaTaifaMinijuegos");
+        }
+    }
+
+}
