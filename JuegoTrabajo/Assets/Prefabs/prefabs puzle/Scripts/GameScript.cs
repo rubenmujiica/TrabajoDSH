27522789211ea4a0c68020828e7c2aa976fed2cb
JaimeCorrales <jaime.corralesdelgado@alum.uca.es>
2024-05-18 19:55:50 +0200

    PuzleTerminado

--- JuegoTrabajo/Assets/Prefabs/prefabs puzle/Scripts/GameScript.cs
+++ JuegoTrabajo/Assets/Prefabs/prefabs puzle/Scripts/GameScript.cs
@@ -1,17 +1,23 @@
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameScript : MonoBehaviour
 {
     [SerializeField]private Transform emptySpace = null;
     private Camera _camera;
     [SerializeField] private TileScript[] tiles;
+    private int emptySpaceIndex = 15;
+    private bool _isFinished;
+    [SerializeField] private GameObject endPanel;
+    [SerializeField] private Text endPanelTimeText;
 
     void Start()
     {
         _camera = Camera.main;
-        Shuffle();
+        //Shuffle();
     }
 
     // Update is called once per frame
@@ -26,13 +32,47 @@ public class GameScript : MonoBehaviour
                     TileScript thisTile = hit.transform.GetComponent<TileScript>(); 
                     emptySpace.position = thisTile.targetPosition;
                     thisTile.targetPosition = lastEmptySpacePosition;
+                    int tileIndex = findIndex(thisTile);
+                    tiles[emptySpaceIndex] = tiles[tileIndex];
+                    tiles[tileIndex] = null;
+                    emptySpaceIndex = tileIndex;
                 }
             }  
        } 
+       if(_isFinished == false){
+        int correctTiles = 0;
+        foreach(var a in tiles){
+            if(a != null){
+                if(a.inRightPlace)
+                correctTiles++;
+            }
+        }
+        if(correctTiles == tiles.Length-1){
+            _isFinished = true;
+            endPanel.SetActive(true);
+            var a = GetComponent<TimerScript>();
+            a.StopTimer();
+            endPanelTimeText.text = (a.minutes<10 ? "0":"") + a.minutes + ":" + (a.seconds<10 ? "0":"") + a.seconds;
+         }
+      }
+       
+    }
+    public void EndGame(){
+        SceneManager.LoadScene("Mapa");
     }
 
     public void Shuffle(){
-        for(int i = 0; i<=14; i++){
+        if(emptySpaceIndex != 15){
+            var tileOn15LastPos = tiles[15].targetPosition;
+            tiles[15].targetPosition = emptySpace.position;
+            emptySpace.position = tileOn15LastPos;
+            tiles[emptySpaceIndex] = tiles[15];
+            tiles[15] = null;
+            emptySpaceIndex = 15;
+        }
+        int invertion;
+        do{
+            for(int i = 0; i<=14; i++){
                 var lastPos = tiles[i].targetPosition;
                 int randomIndex = Random.Range(0, 14);
                 tiles[i].targetPosition = tiles[randomIndex].targetPosition;
@@ -40,7 +80,37 @@ public class GameScript : MonoBehaviour
                 var tile = tiles[i];
                 tiles[i] = tiles[randomIndex];
                 tiles[randomIndex] = tile;
+        }  
+        invertion = GetInversions();
+        Debug.Log("");      
+        }while(invertion%2 != 0);
+        }
+
+        int GetInversions(){
+            int InversionSum = 0;
+            for(int i = 0; i<tiles.Length; i++){
+                int thisTileInvertion = 0;
+                for(int j = i; j<tiles.Length; j++){
+                    if(tiles[j] != null){
+                        if(tiles[i].number > tiles[j].number){
+                            thisTileInvertion++;
+                        }
+                    }
+                }
+                InversionSum += thisTileInvertion;
             }
+            return InversionSum;    
+        }
+
+        public int findIndex(TileScript ts){
+            for(int i = 0;i < tiles.Length; i++){
+                if(tiles[i]!=null){
+                    if(tiles[i] == ts){
+                        return i;
+                    }
+                }
+            }
+            return -1;
         }
 }
 
--- JuegoTrabajo/Assets/Prefabs/prefabs puzle/Scripts/TileScript.cs
+++ JuegoTrabajo/Assets/Prefabs/prefabs puzle/Scripts/TileScript.cs
@@ -5,7 +5,10 @@ public class TileScript : MonoBehaviour
     public Vector3 targetPosition;
     private Vector3 correctPosition;
     private SpriteRenderer _sprite;
-    void Start()
+    public int number;
+    public bool inRightPlace;
+
+    void Awake()
     {
         targetPosition = transform.position;
         correctPosition = targetPosition;
@@ -18,9 +21,12 @@ public class TileScript : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, targetPosition, 0.05f);
         if (targetPosition == correctPosition){
             _sprite.color = Color.green;
+            inRightPlace = true;
         }
         else{
             _sprite.color = Color.white;
+            inRightPlace = false;
         }
     }
+    
 }
