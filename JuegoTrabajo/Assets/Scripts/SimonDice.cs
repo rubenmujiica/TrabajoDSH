6d616b0b6395d6b97a14b3a6a80d9d8ca30b8455
MarcoJMB <marcojose.mirandabahamonde@alum.uca.es>
2024-05-20 01:46:59 +0200

    Mas audios

--- JuegoTrabajo/Assets/Scripts/SimonDice.cs
+++ JuegoTrabajo/Assets/Scripts/SimonDice.cs
@@ -5,15 +5,5 @@ using UnityEngine.UI;
 
 public class SimonDice : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
